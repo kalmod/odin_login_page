19aa69554884642fb86c7a13e5fcaa1237aa38bd
kalmod <kalmod@live.com>
2023-05-07 15:19:38 -0400

    Added final styling and custom validation to form.

--- README.md
+++ README.md
@@ -20,12 +20,25 @@ I need to create and style a form.
             - Invalid fields should have a red background.
             - Javascript should be used to validate the password fields.
         - Submit button
+            - Create custom validation. This will be used to accomidate the passwords not matching.
+            - Also change the error message, because why not.
+            - setCustomValidity must be empty to if true
+            - I can use the Validity state to check if input is valid if I want to customize the message. 
+            - That's because changing the error message changes the validity state. I'll need to use other criteria to manipulate whether the form is valid.
+            - I can use regex test to check the input instead of relying on pattern.
         - Already log in field
     - Try to keep a theme.
 
-Resources:
+##### Resources:
+
 [Project Page](https://www.theodinproject.com/lessons/node-path-intermediate-html-and-css-sign-up-form)
+
 [SVG Repo](https://www.svgrepo.com/svg/366230/keyboard-shortcut)
+
 [Input Patterns](https://www.w3schools.com/tags/att_input_pattern.asp)
+
 [Validate email with JS](https://ui.dev/validate-email-address-javascript)
+
 [setCustomValidity](https://developer.mozilla.org/en-US/docs/Web/API/HTMLObjectElement/setCustomValidity)
+
+[HTML Input Validation with JavaScript](https://www.aleksandrhovhannisyan.com/blog/html-input-validation-without-a-form/#option-2-custom-error-messaging)
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const inputFields = document.querySelectorAll('.entry-container > input');
+const signupForm = document.querySelector('#odin-sign-up-form');
+const formButton = document.querySelector('.button-container > button');
 
 inputFields.forEach((inputField,index) =>{
     inputField.addEventListener('focusout',function (event) {
@@ -17,42 +19,79 @@ inputFields.forEach((inputField,index) =>{
 
 //.isInvalid & .password-error
 
-const password1 = document.querySelector('#password1')
-const password2 = document.querySelector('#password2')
+// const password1 = document.querySelector('#password1')
+// const password2 = document.querySelector('#password2')
+
+
+signupForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    if (validateForm()){
+        signupForm.submit();
+    }
+});
+formButton.addEventListener('click', (event)=>{
+    if (validateForm()){
+        signupForm.submit();
+    }
+})
+
 
 function checkifPasswordsMatch(p1,p2){
-    const containerDiv = document.querySelector('.entry-container:nth-child(5)');
-
+    // const containerDiv = document.querySelector('.entry-container:nth-child(5)');
+    let validPassword = true;
     if (p1.value.length !== 0 && p2.value.length !== 0 && p1.value !== p2.value){
         p1.classList.add('isInvalid');
         p2.classList.add('isInvalid');
         document.documentElement.style.setProperty('--password-error-opacity','1');
+        validPassword = false;
     } else{
         p1.classList.remove('isInvalid');
         p2.classList.remove('isInvalid');
         document.documentElement.style.setProperty('--password-error-opacity','0');
     }
-    console.log(passwordEMOpacity);
+    return validPassword;
 }
 
-password1.addEventListener('focusout',(event)=>{
-    checkifPasswordsMatch(password1,password2);
-});
+// password1.addEventListener('focusout',(event)=>{
+//     checkifPasswordsMatch(password1,password2);
+// });
 
-password2.addEventListener('focusout',(event)=>{
-    checkifPasswordsMatch(password1,password2);
-});
+// password2.addEventListener('focusout',(event)=>{
+//     checkifPasswordsMatch(password1,password2);
+// });
 
 function validateForm(){
     const firstName = document.getElementById("first_name");
     const lastName = document.getElementById("last_name");
-    const email = document.getElementById("element");
+    const email = document.getElementById("email_addr");
     const phoneNumber = document.getElementById("phone_number");
     const p1 = document.querySelector('#password1');
     const p2 = document.querySelector('#password2');
 
     let isValid = true;
-    if (firstName.value === '' ){
+    if ( ! firstName.validity.valid ){
+        isValid = false;
+
+    }
+    if (! lastName.validity.valid ){
         isValid = false;
     }
+    if (! email.validity.valid) {
+        isValid = false;
+    }
+    if (! phoneNumber.validity.valid) {
+        isValid = false;
+    }
+    if (! p1.validity.valid) {
+        isValid = false;
+    }
+    if (! p2.validity.valid) {
+        isValid = false;
+    }
+    if (! checkifPasswordsMatch(p1,p2) ){
+        isValid = false;
+    }
+    firstName.reportValidity();
+    return isValid
 }
+
